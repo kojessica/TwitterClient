ba7843474e6798aaa4a1b830b58398f09f8fc089
Jessica Ko <ko.jessica@gmail.com>
2014-03-28 13:29:20 -0700

    Initial Commit

---
+++ TwitterClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterClient
+//
+//  Created by Jessica Ko on 3/28/14.
+//  Copyright (c) 2014 Jessica Ko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
